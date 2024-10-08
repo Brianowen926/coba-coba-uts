6e7351ee6aab915e1a8d67e578ac75b44f58f757
Brianowen926 <93138817+Brianowen926@users.noreply.github.com>
2024-10-08 15:24:28 +0700

    coba coba

---
+++ app/src/main/java/com/example/cobacobauts/view/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cobacobauts.view.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cobacobauts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
